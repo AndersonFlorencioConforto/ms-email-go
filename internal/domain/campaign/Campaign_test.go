86c958cbf08480a40336277737915b09795e9675
AndersonFlorencioConforto <afcaconforto@gmail.com>
2023-11-06 18:45:19 -0300

    refatora testes de unidades usando a lib testify

--- internal/domain/campaign/Campaign_test.go
+++ internal/domain/campaign/Campaign_test.go
@@ -1,31 +1,43 @@
 package campaign
 
 import (
-	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestNewCampaign(t *testing.T) {
+
+	//arrange
+	var assert = assert.New(t)
 	const aName string = "Test Campaign"
 	const aContent string = "Test Content"
 	var aContacts = []string{"anderson@gmail", "babi@gmail"}
 
+	//act
 	var campaign = newCampaign(aName, aContent, aContacts)
 
-	if campaign.ID != "1" {
-		t.Errorf("Expected campaign.ID to be 1, got %s", campaign.ID)
-	}
-	if campaign.Name != aName {
-		t.Errorf("Expected campaign.Name to be %s, got %s", aName, campaign.Name)
-	}
-	if campaign.Content != aContent {
-		t.Errorf("Expected campaign.Content to be %s, got %s", aContent, campaign.Content)
-	}
+	//assert
+	assert.Equal("1", campaign.ID)
+	assert.Equal(aName, campaign.Name)
+	assert.Equal(aContent, campaign.Content)
+	assert.Equal(len(aContacts), len(campaign.Contacts))
+	assert.Equal(campaign.CreatedAt, time.Now())
 
-	fmt.Println(campaign.Contacts)
-	if len(campaign.Contacts) != len(aContacts) {
-		t.Errorf("Expected campaign.Contacts to have %d items, got %d", len(aContacts), len(campaign.Contacts))
-
-	}
+	//if campaign.ID != "1" {
+	//	t.Errorf("Expected campaign.ID to be 1, got %s", campaign.ID)
+	//}
+	//if campaign.Name != aName {
+	//	t.Errorf("Expected campaign.Name to be %s, got %s", aName, campaign.Name)
+	//}
+	//if campaign.Content != aContent {
+	//	t.Errorf("Expected campaign.Content to be %s, got %s", aContent, campaign.Content)
+	//}
+	//
+	//fmt.Println(campaign.Contacts)
+	//if len(campaign.Contacts) != len(aContacts) {
+	//	t.Errorf("Expected campaign.Contacts to have %d items, got %d", len(aContacts), len(campaign.Contacts))
+	//
+	//}
 
 }
