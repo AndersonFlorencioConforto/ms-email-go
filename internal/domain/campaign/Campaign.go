7c47b1478a354c5a6796c01b4486b6d3cd5e122a
AndersonFlorencioConforto <afcaconforto@gmail.com>
2023-11-06 18:12:55 -0300

    cria constructor de campanha

--- internal/domain/campaign/Campaign.go
+++ internal/domain/campaign/Campaign.go
@@ -13,3 +13,23 @@ type Campaign struct {
 	Content   string
 	Contacts  []Contact
 }
+
+func newCampaign(name string, content string, emails []string) *Campaign {
+	contacts := make([]Contact, len(emails))
+
+	for _, email := range emails {
+		contacts = append(contacts, Contact{
+			Email: email,
+		})
+
+	}
+
+	return &Campaign{
+		ID:        "1",
+		Name:      name,
+		CreatedAt: time.Now(),
+		Content:   content,
+		Contacts:  contacts,
+	}
+
+}
